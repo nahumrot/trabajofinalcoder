fc1ea7806f024328ee4abfc5f6d40c449d7ac98c
nahumrud <nahumrud@hotmail.com>
2022-09-07 22:48:49 -0300

    todo

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import { useCartContext } from '../context/CartContext'
 import {Link} from 'react-router-dom';
 import ItemCart from './ItemCart';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
 
 export default function Cart() {
   const {cart, totalPrice} = useCartContext();
+
+  const order = {
+    buyer:{
+      name: 'nahum',
+      email: 'nahum@rud.com',
+      phone: '6987452'
+    },
+    items: cart.map(product => ({ id:product.id, name: product.id, price: product.price, quantity: product.quantity})),
+    total: totalPrice(),
+  }
+
+  const handClick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db,'orders');
+    addDoc(ordersCollection, order)
+  }
 
   if (cart.length === 0) {
     return (
@@ -21,8 +38,9 @@ export default function Cart() {
             cart.map(product => <ItemCart key={product.id} product={product} />)
         }
         <p>
-            total: {totalPrice}
+            total: {totalPrice()}
         </p>
+        <button onClick={handClick}>Realizar compra</button>
     </>
   )
 }
